7d37bdeb3af81de0af61e5742fc356616bd402bb
Jefferson Pinheiro <jeffersonpinheiro194@gmail.com>
2024-05-21 19:28:26 -0300

    Adicionada nova funcionalidade
    -Barra de Vida de player e enemy adicionadas
    -Div para timer adicionado

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Sprite {
         }
         this.color = color
         this.isAttacking
+        this.health = 100
     }
 
     draw() {
@@ -166,7 +167,8 @@ function animate(){
         player.isAttacking
     ){
         player.isAttacking = false
-        console.log('PlayerHit')
+        enemy.health -= 20
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
     }
     if(
         rectangularCollision({
@@ -176,7 +178,8 @@ function animate(){
         enemy.isAttacking
     ){
         enemy.isAttacking = false
-        console.log('EnemyHit')
+        player.health -= 20
+        document.querySelector('#playerHealth').style.width = player.health + '%'
     }
 }
 
